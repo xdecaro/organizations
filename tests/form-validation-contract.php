5bdc9f3bce2ee5aaf61012485f14463c1d44b0d1
xdecaro <yvv4jfzkbb@privaterelay.appleid.com>
2026-09-13 23:54:23 +0200

    chore: keep validation regression work on feature branch

--- tests/form-validation-contract.php
+++
@@ -1,9 +0,0 @@
-<?php
-$template = file_get_contents(__DIR__ . '/../component/admin/tmpl/organization/edit.php');
-
-if (!str_contains($template, "HTMLHelper::_('behavior.formvalidator')")) {
-    fwrite(STDERR, "Organization edit form must load Joomla formvalidator behavior\n");
-    exit(1);
-}
-
-echo "Organization form validation contract OK\n";
