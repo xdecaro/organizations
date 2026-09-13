a789f9fde25a28ee58d37fe44f7e3412b99f553a
xdecaro <yvv4jfzkbb@privaterelay.appleid.com>
2026-09-13 23:49:05 +0200

    test: make controller route contract formatting-independent

--- tests/form-controller-option-contract.php
+++ tests/form-controller-option-contract.php
@@ -1,12 +1,13 @@
 <?php
 $controller = file_get_contents(__DIR__ . '/../component/admin/src/Controller/OrganizationController.php');
+$compact = preg_replace('/\s+/', '', $controller);
 
 foreach ([
-    "protected \$option = 'com_xdecaroorganizations';",
-    "protected \$view_item = 'organization';",
-    "protected \$view_list = 'organizations';",
+    "protected\$option='com_xdecaroorganizations';",
+    "protected\$view_item='organization';",
+    "protected\$view_list='organizations';",
 ] as $required) {
-    if (!str_contains($controller, $required)) {
+    if (!str_contains($compact, $required)) {
         fwrite(STDERR, "Missing OrganizationController contract: {$required}\n");
         exit(1);
     }
