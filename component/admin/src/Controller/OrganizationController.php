6412082a075a8c452ecc1a91356d39d38aebfa2f
xdecaro <yvv4jfzkbb@privaterelay.appleid.com>
2026-09-13 23:47:35 +0200

    fix: route organization form through real component option

--- component/admin/src/Controller/OrganizationController.php
+++ component/admin/src/Controller/OrganizationController.php
@@ -1,3 +1,3 @@
 <?php
 namespace xdecaro\Component\Organizations\Administrator\Controller;
-defined('_JEXEC') or die;use Joomla\CMS\MVC\Controller\FormController;final class OrganizationController extends FormController{protected $view_list='organizations';}
+defined('_JEXEC') or die;use Joomla\CMS\MVC\Controller\FormController;final class OrganizationController extends FormController{protected $option='com_xdecaroorganizations';protected $view_item='organization';protected $view_list='organizations';}
