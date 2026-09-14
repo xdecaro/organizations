9dff4496b22c33e5fe70689bb07e30d053a364b6
xdecaro <yvv4jfzkbb@privaterelay.appleid.com>
2026-09-14 19:26:48 +0200

    Fix Organizations state actions and redirects
    Map Joomla's published state to Organizations state, keep bulk-action redirects on com_xdecaroorganizations, add regression coverage, and prepare Organizations 1.0.6.

--- component/admin/src/Controller/OrganizationsController.php
+++ component/admin/src/Controller/OrganizationsController.php
@@ -1,3 +1,17 @@
 <?php
+
 namespace xdecaro\Component\Organizations\Administrator\Controller;
-defined('_JEXEC') or die;use Joomla\CMS\MVC\Controller\AdminController;final class OrganizationsController extends AdminController{public function getModel($name='Organization',$prefix='Administrator',$config=['ignore_request'=>true]){return parent::getModel($name,$prefix,$config);}}
+
+defined('_JEXEC') or die;
+
+use Joomla\CMS\MVC\Controller\AdminController;
+
+final class OrganizationsController extends AdminController
+{
+    protected $option = 'com_xdecaroorganizations';
+
+    public function getModel($name = 'Organization', $prefix = 'Administrator', $config = ['ignore_request' => true])
+    {
+        return parent::getModel($name, $prefix, $config);
+    }
+}
