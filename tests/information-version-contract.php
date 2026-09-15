336e916857aba95b7261f98d80783a1c29880082
xdecaro <yvv4jfzkbb@privaterelay.appleid.com>
2026-09-15 22:33:56 +0200

    test: verify build-time asset version synchronization

--- tests/information-version-contract.php
+++ tests/information-version-contract.php
@@ -38,7 +38,9 @@ if (!is_array($asset) || ($asset['version'] ?? '') !== $version) {
     exit(1);
 }
 
-if (!str_contains($build, 'joomla.asset.json') || !str_contains($build, 'data["version"] = version')) {
+if (!str_contains($build, 'joomla.asset.json')
+    || !str_contains($build, '$data["version"] = $version;')
+    || !str_contains($build, '"$WORK/component/joomla.asset.json" "$VERSION"')) {
     fwrite(STDERR, "Build must synchronize joomla.asset.json version from VERSION before packaging.\n");
     exit(1);
 }
