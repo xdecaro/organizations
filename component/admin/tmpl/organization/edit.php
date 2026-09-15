56480ae3d001dcba5ac1b94f3b6dba2da8a5d390
xdecaro <yvv4jfzkbb@privaterelay.appleid.com>
2026-09-15 21:05:09 +0200

    Increase organization edit heading size

--- component/admin/tmpl/organization/edit.php
+++ component/admin/tmpl/organization/edit.php
@@ -15,7 +15,7 @@ $organizationHeading = $organizationName !== ''
 <form action="<?php echo Route::_('index.php?option=com_xdecaroorganizations&layout=edit&id=' . (int) ($this->item->id ?? 0)); ?>" method="post" name="adminForm" id="organization-form" class="form-validate">
     <div class="xdecaro-scope">
         <div class="mb-3">
-            <h2 class="h4 mb-0"><?php echo $organizationHeading; ?></h2>
+            <h2 class="h3 mb-0"><?php echo $organizationHeading; ?></h2>
         </div>
         <?php
         echo HTMLHelper::_('uitab.startTabSet', 'organizationTabs', ['active' => 'identity']);
