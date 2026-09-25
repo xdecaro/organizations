6e6b0dd4856921358f50bcc2beb5d3da8f3fc41d
xdecaro <yvv4jfzkbb@privaterelay.appleid.com>
2026-09-25 20:58:51 +0200

    Support incoming affiliation queries

--- component/admin/src/Model/OrganizationAffiliationsModel.php
+++ component/admin/src/Model/OrganizationAffiliationsModel.php
@@ -11,10 +11,18 @@ use xdecaro\Component\Organizations\Administrator\Service\OrganizationAffiliatio
 final class OrganizationAffiliationsModel extends ListModel
 {
     private int $organizationId = 0;
+    private int $targetOrganizationId = 0;
 
     public function setOrganizationId(int $organizationId): void
     {
         $this->organizationId = max(0, $organizationId);
+        $this->targetOrganizationId = 0;
+    }
+
+    public function setTargetOrganizationId(int $organizationId): void
+    {
+        $this->targetOrganizationId = max(0, $organizationId);
+        $this->organizationId = 0;
     }
 
     protected function getListQuery()
@@ -26,19 +34,30 @@ final class OrganizationAffiliationsModel extends ListModel
                 't.name AS target_name',
                 't.code AS target_code',
                 't.type AS target_type',
+                's.name AS source_name',
+                's.code AS source_code',
+                's.type AS source_type',
             ])
             ->from($db->quoteName('#__xdecaroorganizations_affiliations', 'a'))
             ->leftJoin($db->quoteName('#__xdecaroorganizations_organizations', 't') . ' ON t.id = a.target_organization_id')
-            ->where($db->quoteName('a.state') . ' >= 0')
-            ->order($db->quoteName('a.status') . ' ASC, ' . $db->quoteName('t.name') . ' ASC');
+            ->leftJoin($db->quoteName('#__xdecaroorganizations_organizations', 's') . ' ON s.id = a.organization_id')
+            ->where($db->quoteName('a.state') . ' >= 0');
 
-        if ($this->organizationId < 1) {
-            return $query->where('1 = 0');
+        if ($this->organizationId > 0) {
+            return $query
+                ->where($db->quoteName('a.organization_id') . ' = :organizationId')
+                ->bind(':organizationId', $this->organizationId, ParameterType::INTEGER)
+                ->order($db->quoteName('a.status') . ' ASC, ' . $db->quoteName('t.name') . ' ASC');
         }
 
-        return $query
-            ->where($db->quoteName('a.organization_id') . ' = :organizationId')
-            ->bind(':organizationId', $this->organizationId, ParameterType::INTEGER);
+        if ($this->targetOrganizationId > 0) {
+            return $query
+                ->where($db->quoteName('a.target_organization_id') . ' = :targetOrganizationId')
+                ->bind(':targetOrganizationId', $this->targetOrganizationId, ParameterType::INTEGER)
+                ->order($db->quoteName('a.status') . ' ASC, ' . $db->quoteName('s.name') . ' ASC');
+        }
+
+        return $query->where('1 = 0');
     }
 
     public function getItems(): array
