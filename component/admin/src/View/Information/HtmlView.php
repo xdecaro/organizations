410eb671cad629e48b6062694d7ddfdceb45704d
xdecaro <yvv4jfzkbb@privaterelay.appleid.com>
2026-09-27 00:17:24 +0200

    feat: wire rich information diagnostics view

--- component/admin/src/View/Information/HtmlView.php
+++ component/admin/src/View/Information/HtmlView.php
@@ -1,3 +1,46 @@
 <?php
+
 namespace xdecaro\Component\Organizations\Administrator\View\Information;
-defined('_JEXEC') or die;use Joomla\CMS\Factory;use Joomla\CMS\Language\Text;use Joomla\CMS\MVC\View\HtmlView as BaseHtmlView;use Joomla\CMS\Toolbar\ToolbarHelper;final class HtmlView extends BaseHtmlView{public array $diagnostics=[];public function display($tpl=null):void{if(!Factory::getApplication()->getIdentity()->authorise('core.manage','com_xdecaroorganizations'))throw new \RuntimeException(Text::_('JERROR_ALERTNOAUTHOR'),403);$this->diagnostics=(array)$this->get('Diagnostics');ToolbarHelper::title(Text::_('COM_XDECAROORGANIZATIONS_INFORMATION'),'info-circle');parent::display($tpl);}}
+
+defined('_JEXEC') or die;
+
+use Joomla\CMS\Factory;
+use Joomla\CMS\Language\Text;
+use Joomla\CMS\MVC\View\HtmlView as BaseHtmlView;
+use Joomla\CMS\Toolbar\ToolbarHelper;
+use xdecaro\Component\Organizations\Administrator\Extension\OrganizationsComponent;
+
+final class HtmlView extends BaseHtmlView
+{
+    public array $diagnostics = [];
+    public bool $canManageInstaller = false;
+
+    public function display($tpl = null): void
+    {
+        $app = Factory::getApplication();
+        $identity = $app->getIdentity();
+
+        if (!$identity->authorise('core.manage', 'com_xdecaroorganizations')) {
+            throw new \RuntimeException(Text::_('JERROR_ALERTNOAUTHOR'), 403);
+        }
+
+        $app->getLanguage()->load(
+            'com_xdecaroorganizations.dashboard',
+            JPATH_ADMINISTRATOR . '/components/com_xdecaroorganizations',
+            null,
+            true
+        );
+
+        $this->diagnostics = (array) $this->get('Diagnostics');
+        $this->canManageInstaller = $identity->authorise('core.manage', 'com_installer');
+
+        $component = $app->bootComponent('com_xdecaroorganizations');
+        if ($component instanceof OrganizationsComponent) {
+            $component->getCoreIntegrationService()->enableUi($this->document->getWebAssetManager());
+        }
+        $this->document->getWebAssetManager()->useStyle('com_xdecaroorganizations.admin');
+
+        ToolbarHelper::title(Text::_('COM_XDECAROORGANIZATIONS_INFORMATION'), 'info-circle');
+        parent::display($tpl);
+    }
+}
